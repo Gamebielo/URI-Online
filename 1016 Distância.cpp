d61e5978c7b14b3ae452ef595ff0afc573c1f805
Gabriel Carvalho <36541377+Gamebielo@users.noreply.github.com>
2018-11-30 14:51:28 -0200

    C++

---
+++ 1016 Distância.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // Biblioteca 'stdio.h' evoluída do C++
+#include <stdlib.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int dist;
+
+    cin >> dist;
+
+    cout << dist*2 << " minutos" << endl;
+
+    return 0;
+}
